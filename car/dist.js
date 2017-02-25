16c16c2c3f1deedb3a67106ad1d10d5be29666d6
jvalin <john.a.valin@gmail.com>
2017-02-25 11:38:36 -0600

    Got callbacks to work right!

--- car/dist.js
+++ car/dist.js
@@ -1,9 +1,9 @@
-function dist(loc) {
+function dist(loc, cb) {
 var request = require('superagent');
 
 var usrloc = loc; //"40.1020,-88.2272";
 var hackloc = "36.1447,-86.8027";
-var key = "AIzaSyB-2Z36S6feZ_8Tt4OR0B8W6q9DM83zLnc";
+var key = "API KEY";
 
 var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
 url += '&origins=' + usrloc + '&destinations=' + hackloc + '&key=' + key;
@@ -18,9 +18,8 @@ if (err) throw err;
 else {
 //console.log(res.text);
 data=res.body;
-//console.log(data.text);
-console.log(data.rows[0].elements[0].distance.value);
-return data.rows[0].elements[0].distance.value;
+//console.log(data.rows[0].elements[0].distance.value);
+cb(data.rows[0].elements[0].distance.value);
 }
 });
 //return data.rows[0].elements[0].distance.value;
