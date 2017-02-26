88711be4f118280f9190815b4dee68e8ed7f43c6
rsun0 <kartboy500@gmail.com>
2017-02-26 10:13:42 -0600

    Allowed preflight

--- server/index.js
+++ server/index.js
@@ -8,6 +8,21 @@ var app = express();
 //var usrloc = "40.1020,-88.2272"; // UIUC to test
 //var vin = "2HGFB2F50DH537943";
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res) {
     var usrloc = req.query.loc;
     var vin = req.query.vin;
