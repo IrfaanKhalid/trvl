f24ca17597081e078fc5627cd8a6680baedee33a
Irfaan Khalid <irfaan.khalid@vanderbilt.edu>
2017-02-25 21:17:01 -0600

    Making progress

--- src/static/index.js
+++ src/static/index.js
@@ -1,15 +1,14 @@
 // Required files/packages
 var express = require('express')
-var dist = require('./car/dist.js')
-var gprice = require('./car/gprice.js')
+var dist = require('../data/dist.js')
+var gprice = require('../data/gprice.js')
 
 var app = express();
 
 var usrloc = "40.1020,-88.2272"; // UIUC to test
 
-
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendfile("src/static/index.html");
 })
 
 app.listen(3000, function () {
