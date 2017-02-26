7aac040d0bf2574a09d3d5af804a9a6216ffc7f7
Benjamin Cooper <bencooper222@gmail.com>
2017-02-26 00:29:19 -0600

    Reclassified as server and website

---
+++ server/src/api/car/dist.js
@@ -0,0 +1,30 @@
+function dist(loc, cb) {
+var request = require('superagent');
+
+var usrloc = loc; //"40.1020,-88.2272";
+var hackloc = "36.1447,-86.8027";
+var key = "AIzaSyB-2Z36S6feZ_8Tt4OR0B8W6q9DM83zLnc";
+
+var url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial';
+url += '&origins=' + usrloc + '&destinations=' + hackloc + '&key=' + key;
+
+//console.log(url);
+
+var data;
+
+
+request.get(url, function(err, res){
+if (err) throw err;
+else {
+//console.log(res.text);
+data=res.body;
+//console.log(data.text);
+//console.log(data.rows[0].elements[0].distance.value);
+cb(data.rows[0].elements[0].distance.value);
+}
+});
+//return data.rows[0].elements[0].distance.value;
+}
+
+
+exports.dist = dist;
