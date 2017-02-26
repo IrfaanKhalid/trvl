537bdc42b1cdbf420799c26ca9aeb0ffdd99f0b0
arunarjunakani <arunarjunakani@gmail.com>
2017-02-26 02:21:16 -0600

    Stuff

--- src/static/src.js
+++ src/static/src.js
@@ -1,14 +1,31 @@
 var hackerMode = 0;
 var organizerMode = 0;
+$(document).ready(function() {
+    $("#login").hide();
+});
 
-function enableHacker() {
-    hackerMode = 1;
-    organizerMode = 0;
-    console.log("Hacker mode!");
-}
 
-function enableOrganizer() {
-    organizerMode = 1;
-    hackerMode = 0;
-    console.log("Organizer mode!");
-}
+$(document).ready(function() {
+    $("#hackBtn").click(function() {
+        if (hackerMode == 0) {
+            hackerMode = 1;
+            organizerMode = 0;
+            console.log("Hacker mode!");
+            $("#login").show();
+        } else {
+            hackerMode = 0;
+            $("#login").hide();
+        }
+    });
+
+    $("#orgBtn").click(function() {
+        if (organizerMode == 0) {
+            organizerMode = 1;
+            hackerMode = 0;
+            console.log("Organizer mode!");
+            $("#login").hide();
+        } else {
+            organizerMode = 0;
+        }
+    });
+});
