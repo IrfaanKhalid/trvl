fb7d5d773316961d71712b77eb372067072d973d
arunarjunakani <arunarjunakani@gmail.com>
2017-02-26 05:09:52 -0600

    commit

--- website/real/script.js
+++ website/real/script.js
@@ -10,12 +10,15 @@ $.get("https://hack-travel-2a8a2.firebaseio.com/attendees.json", function(data) 
     //0 = Car, 1 = Bus, 2 = Plane
     if(data[id]["mode"] == 1)
     {
-        busStuff();
+        $.get("https://hack-travel-2a8a2.firebaseio.com/schools.json",function(data){
+            
+        });
+        takingBus();
     }
 });
 
 function busStuff(){
-  
+
 }
 
 
