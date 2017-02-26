4d9af062f7fa937ff0016586f081f962ccb8d5ff
Benjamin Cooper <bencooper222@gmail.com>
2017-02-26 00:25:43 -0600

    Reorganized repo with clear and consistent directories

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 // Required files/packages
 var express = require('express')
-var dist = require('./car/dist.js')
-var gprice = require('./car/gprice.js')
+var dist = require('../data/dist.js')
+var gprice = require('../data/gprice.js')
 
 var app = express();
 
 var usrloc = "40.1020,-88.2272"; // UIUC to test
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.sendfile("src/static/index.html");
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 })
