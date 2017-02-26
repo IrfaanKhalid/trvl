62ec94ff79b7fe2d9265b3e244e251b6ee987d48
arunarjunakani <arunarjunakani@gmail.com>
2017-02-26 01:40:01 -0600

    basic frontend stuff

--- src/static/src.js
+++ src/static/src.js
@@ -1,4 +1,14 @@
-/*var beSad = function()
-{
-  console.log("I'm very, very sad.");
-}*/
+var hackerMode = 0;
+var organizerMode = 0;
+
+function enableHacker() {
+    hackerMode = 1;
+    organizerMode = 0;
+    console.log("Hacker mode!");
+}
+
+function enableOrganizer() {
+    organizerMode = 1;
+    hackerMode = 0;
+    console.log("Organizer mode!");
+}
