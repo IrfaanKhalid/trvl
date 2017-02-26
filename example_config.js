96515dcc0a70dbfd9cd751d88cacc277d0085eec
Benjamin Cooper <bencooper222@gmail.com>
2017-02-25 23:37:08 -0600

    Your API Key

--- example_config.js
+++ example_config.js
@@ -2,11 +2,12 @@ var config = {};
 
 // setup parts to config
 config.firebase = {};
+config.edmunds = {};
 
 // firebase - this is taken directly from Firebase docs
 config.firebase.apiKey = "API KEY";
 config.firebase.databaseURL = "YOUR URL";
 
-
+config.edmunds.apiKey = "API KEY";
 
 module.exports = config;
