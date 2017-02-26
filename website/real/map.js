67203de5d6120e9d1d9e2b64a8ae4a50c4f4ecf1
Benjamin Cooper <bencooper222@gmail.com>
2017-02-26 03:49:47 -0600

    Implemented very basic routing with StreetMap

--- website/real/map.js
+++ website/real/map.js
@@ -6,3 +6,19 @@ var sidebar = document.getElementById('sidebar');
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+
+// EXPECTING LAT LONG ARRAY
+function drawRoute(start, end) {
+    L.Routing.control({
+        waypoints: [
+            L.latLng(start[0], start[1]),
+            L.latLng(end[0], end[1])
+        ]
+    }).addTo(map);
+}
+
+
+var purdue = [40.423313, -86.920529];
+var vanderbilt = [36.142956, -86.805800];
